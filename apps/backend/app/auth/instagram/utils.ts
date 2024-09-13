9efa1d137a1433595aef2dcf12e4793dcc8885c3
Abhinay Pandey <abhinay.pandey@springy.se>
2024-09-13 13:18:51 +0530

    each seller page

--- apps/backend/app/auth/instagram/utils.ts
+++ apps/backend/app/auth/instagram/utils.ts
@@ -1,5 +1,5 @@
 export const getInstagramAuthorizationUrl = (redirectUrl: string) =>
-  `https://www.facebook.com/dialog/oauth?client_id=${process.env.META_CLIENT_ID}&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/_auth/instagram/oauth-redirect&state=${redirectUrl}&response_type=token&scope=instagram_basic,instagram_manage_insights`;
+  `https://www.facebook.com/dialog/oauth?client_id=${process.env.META_CLIENT_ID}&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=${process.env.NEXT_PUBLIC_FRONTEND_BASE_URL}/_auth/instagram/oauth-redirect&state=${redirectUrl}&response_type=token&scope=instagram_basic,instagram_manage_insights,business_management`;
 
 export const getGraphUrl = (
   path: string,
