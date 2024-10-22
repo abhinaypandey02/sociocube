d194fe00b289a181af1cf3dd244c6d149349db28
Abhinay Pandey <abhinay.pandey@springy.se>
2024-10-22 20:46:56 +0530

    constants init

--- apps/backend/instrumentation.ts
+++ apps/backend/instrumentation.ts
@@ -1,20 +1,20 @@
-import categories from "commons/categories";
+// import categories from "commons/categories";
 import { syncPrices } from "./lib/payment/price";
-import { db } from "./lib/db";
-import { CategoryTable, UserTable } from "./app/graphql/types/User/db/schema";
+// import { db } from "./lib/db";
+// import { CategoryTable, UserTable } from "./app/graphql/types/User/db/schema";
 
 export async function register() {
   if (process.env.STRIPE_SECRET_KEY && process.env.NEXT_PUBLIC_DEVELOPMENT) {
     console.warn("SYNCING PRICES");
     await syncPrices();
   }
-  console.warn("SYNCING CATEGORIES");
-  await Promise.all(
-    categories.map(async (category) =>
-      db.insert(CategoryTable).values(category).onConflictDoUpdate({
-        target: UserTable.id,
-        set: category,
-      }),
-    ),
-  );
+  // console.warn("SYNCING CATEGORIES");
+  // await Promise.all(
+  //   categories.map(async (category) =>
+  //     db.insert(CategoryTable).values(category).onConflictDoUpdate({
+  //       target: UserTable.id,
+  //       set: category,
+  //     }),
+  //   ),
+  // );
 }
