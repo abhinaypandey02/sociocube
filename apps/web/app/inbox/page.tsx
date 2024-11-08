f5a8f7309cca6c4ab2570b6be054e9e182713b1f
Abhinay Pandey <abhinay.pandey@springy.se>
2024-11-08 15:04:59 +0530

    inbox

---
+++ apps/web/app/inbox/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <iframe
+      className="size-full h-screen"
+      src="https://inboxkitten.com/inbox/collect-prevent-91/list"
+      title="Inbox"
+    />
+  );
+}
