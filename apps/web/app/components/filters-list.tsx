e2206ab16899066f02cca633f5c23b9e3bb0b421
Abhinay Pandey <abhinay.pandey@springy.se>
2024-11-08 10:42:12 +0530

    colors

--- apps/web/app/components/filters-list.tsx
+++ apps/web/app/components/filters-list.tsx
@@ -39,7 +39,7 @@ export default function FiltersList() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <div>
-            <h2 className="text-base font-semibold leading-7 text-indigo-600">
+            <h2 className="text-base font-semibold leading-7 text-accent">
               Precise discovery
             </h2>
             <p className="mt-2 font-poppins text-3xl font-bold text-gray-900 sm:text-4xl">
@@ -58,7 +58,7 @@ export default function FiltersList() {
                 <dt className="font-poppins font-semibold text-gray-900">
                   <Check
                     aria-hidden="true"
-                    className="absolute left-0 top-1 size-5 text-indigo-500"
+                    className="absolute left-0 top-1 size-5 text-accent"
                   />
                   {feature.name}
                 </dt>
