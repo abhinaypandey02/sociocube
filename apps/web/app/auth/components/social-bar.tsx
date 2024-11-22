84145209af30be9634394b60a48dc7ca1163d011
Abhinay Pandey <abhinay.pandey@springy.se>
2024-11-22 11:07:29 +0530

    sign

--- apps/web/app/auth/components/social-bar.tsx
+++ apps/web/app/auth/components/social-bar.tsx
@@ -1,12 +1,16 @@
+"use client";
 import React from "react";
 import { GoogleLogo, InstagramLogo } from "@phosphor-icons/react/dist/ssr";
+import { useSearchParams } from "next/navigation";
 
 export default function SocialBar() {
+  const params = useSearchParams();
+  const redirectURL = params.get("redirect");
   return (
-    <div className="mt-6">
+    <div className="mt-6 flex gap-3">
       <a
         className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
-        href="/_auth/google"
+        href={`/_auth/google?redirectURL=${redirectURL}`}
       >
         <GoogleLogo size={20} />
         <span className="text-sm font-semibold leading-6">Google</span>
@@ -14,7 +18,7 @@ export default function SocialBar() {
 
       <a
         className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
-        href="/_auth/instagram"
+        href={`/_auth/instagram?redirectURL=${redirectURL}`}
       >
         <InstagramLogo size={20} />
         <span className="text-sm font-semibold leading-6">Instagram</span>
