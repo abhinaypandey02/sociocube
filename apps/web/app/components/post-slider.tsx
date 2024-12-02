757ea793536033c65166778fe23449d3c9cbe607
Abhinay Pandey <abhinay.pandey@springy.se>
2024-12-02 17:15:06 +0530

    nodownload

--- apps/web/app/components/post-slider.tsx
+++ apps/web/app/components/post-slider.tsx
@@ -8,6 +8,7 @@ import type { GetFeaturedSellersQuery } from "../../__generated__/graphql";
 
 import "swiper/css";
 import "swiper/css/effect-cards";
+import Image from "next/image";
 
 function PostSlide({
   post,
@@ -20,7 +21,7 @@ function PostSlide({
       <div>
         <video
           autoPlay
-          className={canPlay ? "" : "size-1"}
+          className={canPlay ? "" : "size-0"}
           controlsList="nodownload"
           height={568}
           loop
@@ -34,7 +35,7 @@ function PostSlide({
           <source src={post.mediaURL || ""} />
         </video>
         {!canPlay && (
-          <img
+          <Image
             alt={post.creatorName}
             height={568}
             src={post.thumbnailURL}
@@ -42,29 +43,33 @@ function PostSlide({
           />
         )}
       </div>
-      <div className="absolute bottom-0 h-20 w-full bg-black/30 blur" />
-      <a
-        className="absolute bottom-4 flex w-full items-center gap-3 pl-4 pr-3 "
-        href={`${getRoute("Profile")}/${post.creatorUsername}`}
-      >
-        <img
-          alt={post.creatorName}
-          className="size-10 rounded-full"
-          src={post.creatorImage}
-        />
-        <p className="grow font-bold text-white">{post.creatorName}</p>
-        <div className="text-xs font-semibold text-white">
-          <div className="flex flex-col  items-center gap-1">
-            <TrendUp size={16} weight="bold" />
-            {Math.round(post.er)}%
-          </div>
+      {canPlay ? (
+        <div className="absolute bottom-0 h-20 w-full bg-black/30 blur" />
+      ) : null}
+      {canPlay ? (
+        <a
+          className="absolute bottom-4 flex w-full items-center gap-3 pl-4 pr-3 "
+          href={`${getRoute("Profile")}/${post.creatorUsername}`}
+        >
+          <img
+            alt={post.creatorName}
+            className="size-10 rounded-full"
+            src={post.creatorImage}
+          />
+          <p className="grow font-bold text-white">{post.creatorName}</p>
+          <div className="text-xs font-semibold text-white">
+            <div className="flex flex-col  items-center gap-1">
+              <TrendUp size={16} weight="bold" />
+              {Math.round(post.er)}%
+            </div>
 
-          <div className=" mt-2 flex flex-col items-center gap-1.5">
-            <Heart size={16} weight="bold" />
-            {post.likes}
+            <div className=" mt-2 flex flex-col items-center gap-1.5">
+              <Heart size={16} weight="bold" />
+              {post.likes}
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
+      ) : null}
     </li>
   );
 }
