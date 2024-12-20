072cc0ac2f89a13a43a5b19382e355a9242c4c17
Abhinay Pandey <abhinay.pandey@springy.se>
2024-12-20 14:46:47 +0530

    404 fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 import { queryGQL } from "../lib/apollo-server";
 import { GET_FEATURED_SELLERS_AND_POSTS } from "../lib/queries";
 import type { GetFeaturedSellersQuery } from "../__generated__/graphql";
 import { getSEO, SEO } from "../constants/seo";
 import { getOrganizationDomain } from "../lib/utils-server";
+import { getRoute } from "../constants/routes";
 import Hero from "./components/hero";
 import TopCreators from "./components/top-creators";
 import Schema from "./components/schema";
 import Faqs from "./components/faqs";
-import ProfilePage, {
-  generateMetadata as profileGenerateMetadata,
-} from "./profile/[username]/page";
 import JobPostings from "./components/job-postings";
 
 const HowItWorks = dynamic(() => import("./components/how-it-works"));
@@ -21,15 +20,7 @@ const Features = dynamic(() => import("./components/features"));
 const AboutUs = dynamic(() => import("./components/about-us"));
 const FiltersList = dynamic(() => import("./components/filters-list"));
 
-export async function generateMetadata(): Promise<Metadata> {
-  const domain = await getOrganizationDomain();
-  const username = domain?.split(".")[0];
-  if (domain)
-    return profileGenerateMetadata({
-      params: new Promise((res) => {
-        res({ username });
-      }),
-    });
+export function generateMetadata(): Metadata {
   return getSEO();
 }
 
@@ -37,16 +28,7 @@ async function HomePage() {
   const domain = await getOrganizationDomain();
   if (domain) {
     const username = domain.split(".")[0];
-    if (username)
-      return (
-        <ProfilePage
-          params={
-            new Promise((res) => {
-              res({ username });
-            })
-          }
-        />
-      );
+    if (username) redirect(`${getRoute("Profile")}/${username}`);
   }
   const { sellers, posts, postings } = await queryGQL(
     GET_FEATURED_SELLERS_AND_POSTS,
