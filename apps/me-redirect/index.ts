10048e620bf3b5cdf02b2138becfbdf6ba8ae3b6
Abhinay Pandey <abhinay.pandey@springy.se>
2024-12-20 15:20:37 +0530

    upload image to our storage

--- apps/me-redirect/index.ts
+++ apps/me-redirect/index.ts
@@ -1,11 +1,13 @@
-export function GET(request: Request) {
-    const domain = request.headers.get('host')
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export function GET(req: VercelRequest, res: VercelResponse) {
+    const domain = req.headers?.host
     if (
         !domain
     )
         return Response.redirect(process.env.NEXT_PUBLIC_FRONTEND_BASE_URL||"https://freeluencers.com");
     const username = domain.split(".")[0];
-    return Response.redirect(process.env.NEXT_PUBLIC_FRONTEND_BASE_URL+'/profile/'+username)
+    return res.redirect(process.env.NEXT_PUBLIC_FRONTEND_BASE_URL+'/profile/'+username)
 }
 
 export const config = {
