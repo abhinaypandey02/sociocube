86f405981b193556f964733474f29587c0284321
Abhinay Pandey <abhinay.pandey@springy.se>
2025-01-15 18:49:16 +0530

    rebranding

--- apps/web/app/manifest.ts
+++ apps/web/app/manifest.ts
@@ -12,14 +12,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#F45B69",
     icons: [
       {
-        src: "/icon.png",
-        sizes: "any",
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
-        src: "/favicon.ico",
-        sizes: "64x64 32x32 24x24 16x16",
-        type: "image/x-icon",
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
