ef0d1c72723b89906ada295420740757f1876f61
Abhinay Pandey <abhinay.pandey@springy.se>
2025-01-24 15:25:02 +0530

    smoother loading

--- apps/web/lib/rich-text.tsx
+++ apps/web/lib/rich-text.tsx
@@ -1,24 +1,26 @@
 import { format } from "@flasd/whatsapp-formatting";
 
 export function renderRichText(text: string) {
-  const lines = format(text);
-  const elements = lines.split(" ");
-  return elements
-    .map((element) => {
-      if (element.startsWith("http")) {
-        return (
-          <a
-            className="text-accent underline"
-            href={element}
-            key={element}
+  const renderedText = format(text);
+  const lines = renderedText.split("<br>");
+  return lines
+    .map((line) =>
+      line
+        .split(" ")
+        .map((element) => {
+          if (element.startsWith("http")) {
+            return `<a
+            class="text-accent underline"
+            href="${element}"
             rel="noopener"
             target="_blank"
           >
-            {element}{" "}
-          </a>
-        );
-      }
-      return element;
-    })
-    .join(" ");
+            ${element}
+          </a>`;
+          }
+          return element;
+        })
+        .join(" "),
+    )
+    .join("<br/>");
 }
