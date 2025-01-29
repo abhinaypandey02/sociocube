fe8aaf734b08abae6a1e724c8980cfb64c37f849
Abhinay Pandey <abhinay.pandey@springy.se>
2025-01-29 12:11:16 +0530

    error boundary

--- apps/web/app/opengraph-image.tsx
+++
@@ -1,7 +0,0 @@
-import { getOgImage } from "../lib/util-components";
-
-export { size } from "../lib/util-components";
-
-export default function Image() {
-  return getOgImage("Connecting brands with influencers", "Search now");
-}
