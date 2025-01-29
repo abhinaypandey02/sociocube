24f76285eeabd5eee1bcbae8eb674c5b2aabdc47
Abhinay Pandey <abhinay.pandey@springy.se>
2025-01-29 13:03:18 +0530

    error boundary

--- apps/backend/app/graphql/types/User/resolvers/queries/get-featured-sellers.ts
+++ apps/backend/app/graphql/types/User/resolvers/queries/get-featured-sellers.ts
@@ -25,7 +25,10 @@ export async function handleGetFeaturedSellers() {
         isNotNull(UserTable.bio),
         isNotNull(UserTable.instagramDetails),
         isNotNull(UserTable.name),
-        inArray(UserTable.id, [372, 541, 458, 361, 556, 422, 408, 469, 450]),
+        inArray(
+          UserTable.id,
+          [372, 541, 458, 361, 556, 422, 408, 469, 450, 148],
+        ),
       ),
     )
     .innerJoin(
