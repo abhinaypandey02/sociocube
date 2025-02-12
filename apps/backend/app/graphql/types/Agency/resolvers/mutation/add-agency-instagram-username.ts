9e7e16186658e93c30be653ab30aa28061f4c2a6
Abhinay Pandey <abhinay.pandey@springy.se>
2025-02-12 11:45:39 +0530

    portfolio

--- apps/backend/app/graphql/types/Agency/resolvers/mutation/add-agency-instagram-username.ts
+++ apps/backend/app/graphql/types/Agency/resolvers/mutation/add-agency-instagram-username.ts
@@ -5,15 +5,48 @@ import { AuthorizedContext } from "../../../../context";
 import GQLError from "../../../../constants/errors";
 import { InstagramDetails } from "../../../Instagram/db/schema";
 import { getInstagramDataExternalAPI } from "../../../../../auth/instagram/utils";
+import { UserTable } from "../../../User/db/schema";
 
 export async function addAgencyInstagramUsername(
   ctx: AuthorizedContext,
   username: string,
 ): Promise<boolean> {
+  const data = await getInstagramDataExternalAPI(username);
+  if (!data?.insta_username) {
+    throw GQLError(
+      500,
+      "Unable to fetch instagram profile, check username and ensure it's a public profile. Also try other method.",
+    );
+  }
+  const [userDetails] = await db
+    .select()
+    .from(UserTable)
+    .where(eq(UserTable.id, ctx.userId))
+    .innerJoin(
+      InstagramDetails,
+      eq(InstagramDetails.id, UserTable.instagramDetails),
+    );
+  if (
+    userDetails?.instagram_data &&
+    userDetails.instagram_data.username === username
+  ) {
+    await db.update(UserTable).set({
+      instagramDetails: null,
+    });
+    await db.insert(AgencyOnboardingTable).values({
+      instagramDetails: userDetails.instagram_data.id,
+      user: ctx.userId,
+      name: data.name || "",
+      photo: data.profile_picture_url,
+      bio: data.biography,
+    });
+    return true;
+  }
   const [existingDetails] = await db
     .select()
     .from(InstagramDetails)
     .where(eq(InstagramDetails.username, username));
+
   if (existingDetails) {
     if (existingDetails.accessToken) {
       throw GQLError(
@@ -27,13 +60,7 @@ export async function addAgencyInstagramUsername(
       );
     }
   }
-  const data = await getInstagramDataExternalAPI(username);
-  if (!data?.insta_username) {
-    throw GQLError(
-      500,
-      "Unable to fetch instagram profile, check username and ensure it's a public profile. Also try other method.",
-    );
-  }
+
   const [details] = await db
     .insert(InstagramDetails)
     .values({
