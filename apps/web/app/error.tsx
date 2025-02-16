57da8ddf669e77c8e8ec712cdf00d23467b443a3
Abhinay Pandey <abhinay.pandey@springy.se>
2025-02-16 19:53:24 +0530

    beauty

---
+++ apps/web/app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { Spinner } from "@phosphor-icons/react/dist/ssr";
+import { Route } from "../constants/routes";
+import { useLogout } from "../lib/auth-client";
+
+export default function Error({ error }: { error: Error }) {
+  const router = useRouter();
+  const path = usePathname();
+  const logout = useLogout();
+  useEffect(() => {
+    void logout();
+    if (path !== Route.Home.toString()) router.push(Route.Home);
+    router.refresh();
+  }, [logout, path, router]);
+  console.error(error);
+  return (
+    <div className="flex min-h-[83vh] items-center justify-center sm:pt-10">
+      <Spinner className="animate-spin fill-primary" size={60} />
+    </div>
+  );
+}
