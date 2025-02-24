900c2ebd8c0ffedb5923e264e5da5c146af5fe44
Abhinay Pandey <abhinay.pandey@springy.se>
2025-02-24 23:57:55 +0530

    reviews

--- apps/backend/app/graphql/types/User/resolvers/mutations/disconnect-instagram.ts
+++ apps/backend/app/graphql/types/User/resolvers/mutations/disconnect-instagram.ts
@@ -4,14 +4,23 @@ import type { AuthorizedContext } from "../../../../context";
 import GQLError from "../../../../constants/errors";
 import { getCurrentUser } from "../../utils";
 import { InstagramDetails } from "../../../Instagram/db/schema";
+import { UserTable } from "../../db/schema";
 
 export async function handleDisconnectInstagram(ctx: AuthorizedContext) {
   const user = await getCurrentUser(ctx);
   if (!user) throw GQLError(403);
-  if (user.instagramDetails)
+  if (user.instagramDetails) {
     await db
-      .update(InstagramDetails)
-      .set({ accessToken: null })
-      .where(eq(InstagramDetails.id, user.instagramDetails));
+      .update(UserTable)
+      .set({
+        instagramDetails: null,
+        isOnboarded: false,
+      })
+      .where(eq(UserTable.id, ctx.userId));
+    if (user.instagramDetails)
+      await db
+        .delete(InstagramDetails)
+        .where(eq(InstagramDetails.id, user.instagramDetails));
+  }
   return true;
 }
