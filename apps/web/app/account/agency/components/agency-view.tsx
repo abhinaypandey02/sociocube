4337cd5291abbd7745d92788461ee2c2a1c4b216
amshal1 <panshal911@gmail.com>
2025-03-06 18:24:15 +0530

    showing left sidebar during loading

--- apps/web/app/account/agency/components/agency-view.tsx
+++ apps/web/app/account/agency/components/agency-view.tsx
@@ -21,7 +21,7 @@ export default function AgencyView({
   loading: boolean;
 }) {
   const [selectedSection, setSelectedSection] = useState(
-    isNaN(defaultSection) ? 0 : defaultSection
+    isNaN(defaultSection) ? 0 : defaultSection,
   );
   const router = useRouter();
   const ACCOUNT_SECTIONS = useAgencySections();
@@ -41,7 +41,7 @@ export default function AgencyView({
                     selectedSection === i
                       ? "bg-gray-50 text-accent"
                       : "text-gray-700 hover:text-accent hover:bg-gray-50",
-                    "group cursor-pointer flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm leading-6 font-semibold"
+                    "group cursor-pointer flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm leading-6 font-semibold",
                   )}
                   onClick={() => {
                     if (item.onClick) {
@@ -51,7 +51,7 @@ export default function AgencyView({
                       router.push(
                         `${getRoute("AccountAgency")}/?section=${i}&agency=${
                           data?.agency.username
-                        }`
+                        }`,
                       );
                     }
                   }}
@@ -63,7 +63,7 @@ export default function AgencyView({
                       selectedSection === i
                         ? "text-accent"
                         : "text-gray-400 group-hover:text-accent",
-                      "h-6 w-6 shrink-0"
+                      "h-6 w-6 shrink-0",
                     )}
                   />
                   {item.title}
