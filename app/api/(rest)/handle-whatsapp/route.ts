5ceca0b3f78405c90a74f4438ed7ac40223d7423
Abhinay Pandey <abhinay.pandey@springy.se>
2025-04-10 18:57:39 +0530

    new hero

--- app/api/(rest)/handle-whatsapp/route.ts
+++ app/api/(rest)/handle-whatsapp/route.ts
@@ -10,30 +10,41 @@ export const POST = async (req: NextRequest) => {
     from: string;
     to: string;
   };
-  console.warn("received body", body);
   if (!body.includes("https://") || !body.includes("forms"))
     return new NextResponse();
-  const posting = await getTransformedPostingData(body);
-  if (posting) {
-    const res = await createPosting(
-      { userId: 134 },
-      {
-        ...posting,
-        deliverables:
-          posting.deliverables.trim() !== ""
-            ? posting.deliverables.trim().split(",")
-            : null,
-        externalLink: posting.externalLink || null,
-        extraDetails: posting.extraDetails || null,
-        platforms: posting.platforms
-          ? [posting.platforms.toLowerCase() as PostingPlatforms]
-          : [PostingPlatforms.INSTAGRAM],
-      },
-    );
-    if (res) {
-      console.warn("Created new posting", res);
-      return new NextResponse(res.toString(), { status: 200 });
+  try {
+    const posting = await getTransformedPostingData(body);
+    if (posting) {
+      const res = await createPosting(
+        { userId: 134 },
+        {
+          ...posting,
+          deliverables:
+            posting.deliverables.trim() !== ""
+              ? posting.deliverables.trim().split(",")
+              : null,
+          externalLink: posting.externalLink || null,
+          extraDetails: posting.extraDetails || null,
+          platforms: posting.platforms
+            ? [posting.platforms.toLowerCase() as PostingPlatforms]
+            : [PostingPlatforms.INSTAGRAM],
+        },
+      );
+      if (res) {
+        return new NextResponse(
+          "Created new posting. https://sociocube.com/campaigns/" + res,
+          { status: 200 },
+        );
+      } else {
+        return new NextResponse(
+          "error while creating posting. it returned null",
+          { status: 500 },
+        );
+      }
+    } else {
+      return new NextResponse("GROK not working proeprly", { status: 500 });
     }
+  } catch (e: unknown) {
+    return new NextResponse((e as Error).message, { status: 500 });
   }
-  return new NextResponse("ERROR CREATING POSTING", { status: 500 });
 };
