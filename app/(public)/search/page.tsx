56bf6079786b9731f1753c3e8c235de68cf24e3a
Abhinay Pandey <abhinaypandey02@gmail.com>
2025-04-17 11:43:17 +0530

    search optimise

--- app/(public)/search/page.tsx
+++ app/(public)/search/page.tsx
@@ -11,25 +11,33 @@ export default async function SearchPage({
 }: {
   searchParams: Promise<Record<string, string>>;
 }) {
-  const params = await searchParams;
-  const filters = {
-    ageRange: parseParams(params.ageRange, "NUMBER") as number,
-    categories: parseParams(params.categories, "ARRAY") as string[],
-    cities: parseParams(params.cities, "NUMERIC_ARRAY") as number[],
-    countries: parseParams(params.countries, "NUMERIC_ARRAY") as number[],
-    states: parseParams(params.states, "NUMERIC_ARRAY") as number[],
-    genders: parseParams(params.genders, "ARRAY") as string[],
-    followersFrom: parseParams(params.followersFrom, "NUMBER") as number,
-    followersTo: parseParams(params.followersTo, "NUMBER") as number,
-    generalPriceFrom: parseParams(params.generalPriceFrom, "NUMBER") as number,
-    generalPriceTo: parseParams(params.generalPriceTo, "NUMBER") as number,
-    query: parseParams(params.query) as string,
-    sortBy: parseParams(params.sortBy) as SearchFilterSorting,
-  };
   return (
     <Injector
       Component={SearchWindow}
-      fetch={() => queryGQL(SEARCH_SELLERS, { filters }, undefined, 120)}
+      fetch={() => {
+        const params = await searchParams;
+        const filters = {
+          ageRange: parseParams(params.ageRange, "NUMBER") as number,
+          categories: parseParams(params.categories, "ARRAY") as string[],
+          cities: parseParams(params.cities, "NUMERIC_ARRAY") as number[],
+          countries: parseParams(params.countries, "NUMERIC_ARRAY") as number[],
+          states: parseParams(params.states, "NUMERIC_ARRAY") as number[],
+          genders: parseParams(params.genders, "ARRAY") as string[],
+          followersFrom: parseParams(params.followersFrom, "NUMBER") as number,
+          followersTo: parseParams(params.followersTo, "NUMBER") as number,
+          generalPriceFrom: parseParams(
+            params.generalPriceFrom,
+            "NUMBER"
+          ) as number,
+          generalPriceTo: parseParams(
+            params.generalPriceTo,
+            "NUMBER"
+          ) as number,
+          query: parseParams(params.query) as string,
+          sortBy: parseParams(params.sortBy) as SearchFilterSorting,
+        };
+        queryGQL(SEARCH_SELLERS, { filters }, undefined, 120);
+      }}
       props={{ filters }}
     />
   );
