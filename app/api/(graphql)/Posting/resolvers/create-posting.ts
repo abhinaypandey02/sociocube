da91a3f3f9a24c123375f58212cbfceabf51d104
Abhinay Pandey <abhinay.pandey@springy.se>
2025-04-20 11:59:25 +0530

    force campaigns

--- app/api/(graphql)/Posting/resolvers/create-posting.ts
+++ app/api/(graphql)/Posting/resolvers/create-posting.ts
@@ -53,31 +53,33 @@ const MAXIMUM_POSTINGS_DAY = 1;
 export async function createPosting(
   ctx: AuthorizedContext,
   newPosting: NewPostingInput,
+  force?: boolean,
 ): Promise<number | null> {
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
-
-  const postings = await db
-    .select({ createdAt: PostingTable.createdAt })
-    .from(PostingTable)
-    .where(
-      and(
-        eq(PostingTable.agency, ctx.userId),
-        gte(PostingTable.createdAt, yesterday),
-      ),
-    );
-  if (postings.length > MAXIMUM_POSTINGS_DAY) {
-    const nextDate = postings.sort(
-      (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
-    )[MAXIMUM_POSTINGS_DAY - 1]?.createdAt;
-    if (nextDate) nextDate.setHours(nextDate.getHours() + 24);
-    const diff = Math.round(
-      ((nextDate?.getTime() || 0) - new Date().getTime()) / (1000 * 60 * 60),
-    );
-    throw GQLError(
-      400,
-      `Only ${MAXIMUM_POSTINGS_DAY} allowed in 24 hours. You can create new posting after ${diff} hours.`,
-    );
+  if (!force) {
+    const postings = await db
+      .select({ createdAt: PostingTable.createdAt })
+      .from(PostingTable)
+      .where(
+        and(
+          eq(PostingTable.agency, ctx.userId),
+          gte(PostingTable.createdAt, yesterday),
+        ),
+      );
+    if (postings.length > MAXIMUM_POSTINGS_DAY) {
+      const nextDate = postings.sort(
+        (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
+      )[MAXIMUM_POSTINGS_DAY - 1]?.createdAt;
+      if (nextDate) nextDate.setHours(nextDate.getHours() + 24);
+      const diff = Math.round(
+        ((nextDate?.getTime() || 0) - new Date().getTime()) / (1000 * 60 * 60),
+      );
+      throw GQLError(
+        400,
+        `Only ${MAXIMUM_POSTINGS_DAY} allowed in 24 hours. You can create new posting after ${diff} hours.`,
+      );
+    }
   }
   try {
     const [posting] = await db
