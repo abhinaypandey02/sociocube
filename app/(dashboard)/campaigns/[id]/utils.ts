e2040f71bc2f2c121bf27787017525aea283abab
Abhinay Pandey <abhinay.pandey@springy.se>
2025-05-01 11:32:11 +0530

    bug fix

--- app/(dashboard)/campaigns/[id]/utils.ts
+++ app/(dashboard)/campaigns/[id]/utils.ts
@@ -5,38 +5,22 @@ import { convertToAbbreviation } from "@/lib/utils";
 import { getAgeGroup, getCurrency } from "../utils";
 
 export function getShareText(posting: NonNullable<GetPostingQuery["posting"]>) {
-  return `*${posting.title}*
-        
-*About* 📝
-${posting.description}
-           
-${
-  posting.deliverables
-    ? `*Deliverables* 📦
-${posting.deliverables.map((deliverable) => `- ${deliverable.trim()}`).join("\n") || ""}`
-    : ""
-}
-        
-*Platform* 📱
-${posting.platforms.join(", ")}
-        
-*Payment* 💳
-${getCurrency(posting.barter, posting.currency, posting.price) || "N/A"}
-        
+  const price = getCurrency(posting.barter, posting.currency, posting.price);
+  return `New collab opportunity for creators!
+  
+${posting.title}
+
+${price}
 ${
   posting.minimumFollowers
-    ? `*Minimum followers* 📈
-${convertToAbbreviation(posting.minimumFollowers)}
-`
+    ? `Followers: >${convertToAbbreviation(posting.minimumFollowers)}`
     : ""
 }
 ${
   posting.minimumAge || posting.maximumAge
-    ? `
-*Age group* 🎂
-
-${getAgeGroup(posting.minimumAge, posting.maximumAge)}`
+    ? `Age: ${getAgeGroup(posting.minimumAge, posting.maximumAge)}`
     : ""
 }
+
 Apply now 🚀 ${getRoute("Campaigns")}/${posting.id}`;
 }
