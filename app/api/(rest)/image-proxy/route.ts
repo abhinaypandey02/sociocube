2b00e06e0b1919b4c2a389092293bbe3e35a3dd0
Abhinay Pandey <abhinay.pandey@springy.se>
2025-05-05 22:21:38 +0530

    bug fixes

--- app/api/(rest)/image-proxy/route.ts
+++ app/api/(rest)/image-proxy/route.ts
@@ -3,5 +3,5 @@ import { NextRequest } from "next/server";
 export const GET = (req: NextRequest) => {
   const url = req.nextUrl.search.split("url=")[1];
   if (!url) return new Response("No URL provided", { status: 400 });
-  return fetch(url);
+  return fetch(decodeURIComponent(url));
 };
