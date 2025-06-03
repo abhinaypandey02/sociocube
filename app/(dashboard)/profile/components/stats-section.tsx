3d49ba8c12b1cf0be3ad358ac0723f335b659ffb
Yash Pandey <pandeyash06@gmail.com>
2025-06-03 18:13:10 +0530

    reverting stats-section to original

--- app/(dashboard)/profile/components/stats-section.tsx
+++ app/(dashboard)/profile/components/stats-section.tsx
@@ -1,8 +1,6 @@
-"use client";
 import React from "react";
 
 import { GetAccountSocialDetailsQuery } from "@/__generated__/graphql";
-import { useToggleSubscribeModal } from "@/lib/auth-client";
 import { convertToAbbreviation } from "@/lib/utils";
 
 import AccountCard from "./account-card";
@@ -11,7 +9,6 @@ export default function StatsSection({
 }: {
   data?: GetAccountSocialDetailsQuery;
 }) {
-  const toggleSubscribeModal = useToggleSubscribeModal();
   const user = data?.user;
   if (!user?.instagramStats) return null;
   return (
@@ -21,56 +18,30 @@ export default function StatsSection({
     >
       <div className="grid grid-cols-3 gap-y-5 gap-x-3 lg:grid-cols-6">
         {[
-          {
-            value: convertToAbbreviation(user.instagramStats?.followers),
-            label: "Followers",
-            suffix: "",
-          },
-          {
-            value: convertToAbbreviation(
-              user.instagramStats?.er === -2 ? -2 : user.instagramStats?.er,
+          [convertToAbbreviation(user.instagramStats?.followers), "Followers"],
+          [convertToAbbreviation(user.instagramStats?.er) + "%", "Engagement"],
+          [
+            convertToAbbreviation(
+              user.instagramStats?.er * user.instagramStats?.followers,
             ),
-            label: "Engagement",
-            suffix: "%",
-          },
-          {
-            value: convertToAbbreviation(
-              user.instagramStats?.followers === -2
-                ? -2
-                : user.instagramStats?.er * user.instagramStats?.followers,
-            ),
-            label: "Reach",
-            suffix: "",
-          },
-          {
-            value: convertToAbbreviation(user.instagramStats?.mediaCount),
-            label: "Media count",
-            suffix: "",
-          },
-          {
-            value: convertToAbbreviation(user.instagramStats?.averageLikes),
-            label: "Avg. Likes",
-            suffix: "",
-          },
-          {
-            value: convertToAbbreviation(user.instagramStats?.averageComments),
-            label: "Avg. Comments",
-            suffix: "",
-          },
-        ].map(({ value, label, suffix }) => (
+            "Reach",
+          ],
+          [
+            convertToAbbreviation(user.instagramStats?.mediaCount),
+            "Media count",
+          ],
+          [
+            convertToAbbreviation(user.instagramStats?.averageLikes),
+            "Avg. Likes",
+          ],
+          [
+            convertToAbbreviation(user.instagramStats?.averageComments),
+            "Avg. Comments",
+          ],
+        ].map(([value, label]) => (
           <div key={label}>
             <p className="text-2xl font-poppins font-medium text-center text-gray-700">
-              {value === "-2" ? (
-                <span
-                  className="blur-xs cursor-pointer"
-                  onClick={() => toggleSubscribeModal()}
-                  title="Subscribe to view this stat"
-                >
-                  XX
-                </span>
-              ) : (
-                value + suffix
-              )}
+              {value}
             </p>
             <p className="text-xs text-gray-500 text-center">{label}</p>
           </div>
