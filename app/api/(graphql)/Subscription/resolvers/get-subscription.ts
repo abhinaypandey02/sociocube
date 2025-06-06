2c8464529275af6a6e4cd7904c56588bf4fdfef2
Abhinay Pandey <abhinay.pandey@springy.se>
2025-06-06 15:38:42 +0530

    sub

--- app/api/(graphql)/Subscription/resolvers/get-subscription.ts
+++ app/api/(graphql)/Subscription/resolvers/get-subscription.ts
@@ -2,21 +2,21 @@ import type { Context } from "@backend/lib/auth/context";
 import { db } from "@backend/lib/db";
 import { eq } from "drizzle-orm";
 
-import { SubscriptionTable } from "../db";
+import { SubscriptionDB, SubscriptionTable } from "../db";
+
+const DEFAULT_SUBSCRIPTION: SubscriptionDB = {
+  subscriptionID: null,
+  plan: null,
+  nextBilling: null,
+  user: -1,
+  status: null,
+};
 
 export async function handleGetSubscription(ctx: Context) {
-  if (!ctx.userId) return null;
+  if (!ctx.userId) return DEFAULT_SUBSCRIPTION;
   const [subscription] = await db
     .select()
     .from(SubscriptionTable)
     .where(eq(SubscriptionTable.user, ctx.userId));
-  return (
-    subscription || {
-      subscriptionID: null,
-      plan: null,
-      nextBilling: null,
-      user: ctx.userId,
-      status: null,
-    }
-  );
+  return subscription || DEFAULT_SUBSCRIPTION;
 }
