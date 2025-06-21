ad1e73deb9e0534e8f5e73c9beb5dc7b8c4ffa12
Yash Pandey <pandeyash06@gmail.com>
2025-06-21 17:42:35 +0530

    share link update

--- app/(dashboard)/campaigns/[id]/utils.ts
+++ app/(dashboard)/campaigns/[id]/utils.ts
@@ -16,7 +16,7 @@ export function getShareText(
     ...(posting.cities || []).map((city) => city.label),
   ];
   const price = getCurrency(posting.barter, posting.currency, posting.price);
-  return `New collab opportunity for ${posting.gender ? posting.gender : "all"} creators!
+  return `New collab opportunity for ${posting.gender ? posting.gender : ""} creators!
 
 ${posting.title}
 
