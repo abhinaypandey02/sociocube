416136369750d6410436de064b231b95eb030399
Yash Pandey <pandeyash06@gmail.com>
2025-06-21 15:13:40 +0530

    link update

--- app/(dashboard)/campaigns/[id]/utils.ts
+++ app/(dashboard)/campaigns/[id]/utils.ts
@@ -1,4 +1,5 @@
 import type { GetAllPostingsQuery } from "@/__generated__/graphql";
+import countriesData from "@/constants/countries";
 import { getRoute } from "@/constants/routes";
 import { convertToAbbreviation } from "@/lib/utils";
 
@@ -7,12 +8,19 @@ import { getAgeGroup, getCurrency } from "../utils";
 export function getShareText(
   posting: NonNullable<GetAllPostingsQuery["postings"][number]>,
 ) {
+  const locationNames = [
+    ...countriesData
+      .filter((c) => posting.countries?.includes(c.value))
+      .map((country) => country.label),
+    ...(posting.states || []).map((state) => state.label),
+    ...(posting.cities || []).map((city) => city.label),
+  ];
   const price = getCurrency(posting.barter, posting.currency, posting.price);
-  return `New collab opportunity for creators!
-  
+  return `New collab opportunity for ${posting.gender ? posting.gender : "all"} creators!
+
 ${posting.title}
 
-${price}${
+${price ? `${price}` : ""}${
     posting.minimumFollowers
       ? `
 Followers: >${convertToAbbreviation(posting.minimumFollowers)}`
@@ -21,6 +29,11 @@ Followers: >${convertToAbbreviation(posting.minimumFollowers)}`
     posting.minimumAge || posting.maximumAge
       ? `
 Age: ${getAgeGroup(posting.minimumAge, posting.maximumAge)}`
+      : ""
+  }${
+    locationNames.length > 0
+      ? `
+Location: ${locationNames.join(", ")}`
       : ""
   }
 
