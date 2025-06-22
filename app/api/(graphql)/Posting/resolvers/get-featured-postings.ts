64fdfc1bad6d67f6fd0cf50934229da40f1911b3
Abhinay Pandey <abhinay.pandey@springy.se>
2025-06-22 23:15:24 +0530

    email fix

--- app/api/(graphql)/Posting/resolvers/get-featured-postings.ts
+++ app/api/(graphql)/Posting/resolvers/get-featured-postings.ts
@@ -5,7 +5,7 @@ import { ApplicationTable } from "../../Application/db";
 import { PostingTable } from "../db";
 
 export async function getFeaturedPostings() {
-  const others = await db
+  return db
     .select(getTableColumns(PostingTable))
     .from(PostingTable)
     .where(
@@ -19,14 +19,4 @@ export async function getFeaturedPostings() {
     .groupBy(PostingTable.id)
     .orderBy(desc(count(ApplicationTable.id)))
     .limit(8);
-  if (others.length === 8) return others;
-  const ours = await db
-    .select(getTableColumns(PostingTable))
-    .from(PostingTable)
-    .where(and(eq(PostingTable.open, true), ne(PostingTable.id, 62)))
-    .innerJoin(ApplicationTable, eq(ApplicationTable.posting, PostingTable.id))
-    .groupBy(PostingTable.id)
-    .orderBy(desc(count(ApplicationTable.id)))
-    .limit(8 - others.length);
-  return [...others, ...ours];
 }
