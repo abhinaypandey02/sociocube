211c6139a5169f980400d6daf959641cc125eab7
Abhinay Pandey <abhinay.pandey@springy.se>
2025-06-29 13:49:05 +0530

    email fix

--- __generated__/gql.ts
+++ __generated__/gql.ts
@@ -41,7 +41,7 @@ const documents = {
     "\n  #graphql\n  mutation SendReviewByUser($args:SendReviewByUserArgs!) {\n    sendReviewByUser(args: $args)  \n  }\n": types.SendReviewByUserDocument,
     "\n  #graphql\n  mutation UnlinkSocialAccount {\n    unlinkSocialAccount\n  }\n": types.UnlinkSocialAccountDocument,
     "\n  #graphql\n  mutation SendAnnouncement($body:String!, $postingID:Float!, $apps:[Float!]){\n    sendAnnouncement(body: $body, postingID: $postingID, apps: $apps)   \n  }\n": types.SendAnnouncementDocument,
-    "\n  #graphql\n  mutation DeleteUser{\n    deleteUser\n  }\n": types.DeleteUserDocument,
+    "\n  #graphql\n  mutation DeleteUser($password:String!){\n    deleteUser(password: $password)\n  }\n": types.DeleteUserDocument,
     "\n  #graphql\n  query GetCurrentUser {\n    user: getCurrentUser {\n      id\n      bio\n      email\n      username\n      name\n      photo\n      role\n      emailVerified\n      isOnboarded\n      instagramStats {\n        username\n        isVerified\n      }\n    }\n  }\n": types.GetCurrentUserDocument,
     "\n  #graphql\n  query GetUserCurrency {\n    user: getCurrentUser {\n        id\n      locationID {\n        country\n      }\n    }\n  }\n": types.GetUserCurrencyDocument,
     "\n  #graphql\n  query GetDefaultOnboardingDetails {\n    getCurrentUser {\n      id\n      email\n      name\n      photo\n      role\n      isOnboarded\n      instagramStats {\n        username\n      }\n      bio\n      username\n      pricing {\n        starting\n      }\n      gender\n      category\n      dob\n      location {\n        city\n        currency\n      }\n      locationID {\n        city\n        country\n      }\n    }\n  }\n": types.GetDefaultOnboardingDetailsDocument,
@@ -199,7 +199,7 @@ export function gql(source: "\n  #graphql\n  mutation SendAnnouncement($body:Str
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "\n  #graphql\n  mutation DeleteUser{\n    deleteUser\n  }\n"): (typeof documents)["\n  #graphql\n  mutation DeleteUser{\n    deleteUser\n  }\n"];
+export function gql(source: "\n  #graphql\n  mutation DeleteUser($password:String!){\n    deleteUser(password: $password)\n  }\n"): (typeof documents)["\n  #graphql\n  mutation DeleteUser($password:String!){\n    deleteUser(password: $password)\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- app/(dashboard)/settings/components/delete-user-modal.tsx
+++ app/(dashboard)/settings/components/delete-user-modal.tsx
@@ -1,11 +1,19 @@
 import React, { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
 import { Button } from "@/components/button";
+import Form from "@/components/form";
+import { Input } from "@/components/input";
 import Modal from "@/components/modal";
-import { useAuthMutation } from "@/lib/apollo-client";
+import { handleGQLErrors, useAuthMutation } from "@/lib/apollo-client";
 import { useLogout } from "@/lib/auth-client";
 import { DELETE_USER } from "@/lib/mutations";
+
+interface FormValues {
+  password: string;
+  prompt: string;
+}
 
 export default function DeleteUserModal({
   isOpen,
@@ -15,6 +23,7 @@ export default function DeleteUserModal({
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) {
   const logout = useLogout();
+  const form = useForm<FormValues>();
 
   const [deleteUser, { loading }] = useAuthMutation(DELETE_USER, {
     onCompleted: () => {
@@ -23,15 +32,38 @@ export default function DeleteUserModal({
       toast.success("Account deleted successfully.");
     },
   });
+
+  function handleSubmit(data: FormValues) {
+    deleteUser({
+      password: data.password,
+    }).catch(handleGQLErrors);
+  }
+
   return (
     <Modal
       open={isOpen}
       close={() => setIsOpen(false)}
       title={"Delete your account?"}
     >
-      <Button loading={loading} onClick={() => deleteUser()}>
-        Confirm
-      </Button>
+      <Form className={"space-y-4 mt-4"} form={form} onSubmit={handleSubmit}>
+        <Input
+          type={"password"}
+          placeholder={"Enter your password"}
+          label={"Your password (Skip if not set)"}
+          name={"password"}
+        />
+        <Input
+          rules={{
+            validate: (value) => value === "delete my account",
+          }}
+          placeholder={"delete my account"}
+          label={'Type "delete my account" to confirm'}
+          name={"prompt"}
+        />
+        <Button type={"submit"} className={"mt-4"} loading={loading}>
+          Confirm
+        </Button>
+      </Form>
     </Modal>
   );
 }
