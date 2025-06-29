2f1ce4e36a6598d07b404234aeb641f7308e1e94
Abhinay Pandey <abhinay.pandey@springy.se>
2025-06-29 13:19:14 +0530

    email fix

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Trash } from "@phosphor-icons/react";
 import { SignOut } from "@phosphor-icons/react/dist/ssr";
 import { useMemo, useState } from "react";
+import { toast } from "react-hot-toast";
 
 import DashboardWrapper from "@/app/(dashboard)/components/dashboard-wrapper";
 import { Button } from "@/components/button";
@@ -10,12 +12,14 @@ import { Route } from "@/constants/routes";
 import { useAuthMutation } from "@/lib/apollo-client";
 import { useLogout } from "@/lib/auth-client";
 import { DELETE_USER } from "@/lib/mutations";
+import { cn } from "@/lib/utils";
 export default function SettingsPage() {
   const logout = useLogout();
-
   const [deleteUser, { loading }] = useAuthMutation(DELETE_USER, {
     onCompleted: () => {
+      logout();
       setConfirmDeleteUser(false);
+      toast.success("Account deleted successfully.");
     },
   });
   const [confirmDeleteUser, setConfirmDeleteUser] = useState(false);
@@ -28,9 +32,10 @@ export default function SettingsPage() {
         onClick: logout,
       },
       {
-        icon: SignOut,
+        icon: Trash,
         title: "Delete account",
         onClick: () => setConfirmDeleteUser(true),
+        className: "text-red-400",
       },
     ],
     [logout],
@@ -50,11 +55,14 @@ export default function SettingsPage() {
           Confirm
         </Button>
       </Modal>
-      <div className="divide-y">
+      <div className="">
         {options.map((option) => (
           <button
             key={option.title}
-            className="py-3 px-4 text-start text-xl hover:bg-gray-100 w-full rounded-xl flex items-center gap-2"
+            className={cn(
+              "py-3 px-4 text-start text-xl hover:bg-gray-100 w-full rounded-xl flex items-center gap-2",
+              option.className,
+            )}
             onClick={option.onClick}
           >
             <option.icon size={24} />
