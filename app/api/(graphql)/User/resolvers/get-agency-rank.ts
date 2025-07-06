e1c6c6d7c2b5cb325096e67421763ef13b151f7a
Yash Pandey <pandeyash06@gmail.com>
2025-07-07 00:53:48 +0530

    agency rank

--- app/api/(graphql)/User/resolvers/get-agency-rank.ts
+++ app/api/(graphql)/User/resolvers/get-agency-rank.ts
@@ -3,56 +3,34 @@ import { desc, eq, getTableColumns, sql } from "drizzle-orm";
 import { and, isNotNull } from "drizzle-orm";
 
 import { Roles } from "@/app/api/lib/constants/roles";
+import { withPagination } from "@/app/api/lib/utils/pagination";
 
 import { ApplicationTable } from "../../Application/db";
 import { PostingTable } from "../../Posting/db";
 import { UserTable } from "../db";
 
 export async function handleGetAgencyRank(page: number) {
-  // return withPagination(
-  //   db
-  //     .select(getTableColumns(UserTable))
-  //     .from(UserTable)
-  //     .leftJoin(PostingTable, eq(PostingTable.agency, UserTable.id))
-  //     .leftJoin(ApplicationTable, eq(ApplicationTable.posting, PostingTable.id))
-  //     .where(
-  //       and(
-  //         isNotNull(UserTable.photo),
-  //         isNotNull(UserTable.bio),
-  //         isNotNull(UserTable.instagramDetails),
-  //         isNotNull(UserTable.name),
-  //         isNotNull(UserTable.username),
-  //         eq(UserTable.role, Roles.Agency)
-  //       )
-  //     )
-  //     .groupBy(UserTable.id)
-  //     .orderBy(
-  //       desc(sql`COUNT(${ApplicationTable.id})`),
-  //       UserTable.id
-  //     ),
-  //   {
-  //     page,
-  //     pageSize: 9,
-  //   }
-  // );
-
-  const featured = db
-    .select(getTableColumns(UserTable))
-    .from(UserTable)
-    .leftJoin(PostingTable, eq(PostingTable.agency, UserTable.id))
-    .leftJoin(ApplicationTable, eq(ApplicationTable.posting, PostingTable.id))
-    .where(
-      and(
-        isNotNull(UserTable.photo),
-        isNotNull(UserTable.bio),
-        isNotNull(UserTable.instagramDetails),
-        isNotNull(UserTable.name),
-        isNotNull(UserTable.username),
-        eq(UserTable.role, Roles.Agency),
-      ),
-    )
-    .groupBy(UserTable.id)
-    .orderBy(desc(sql`COUNT(${ApplicationTable.id})`), UserTable.id)
-    .limit(8);
-  return featured;
+  return withPagination(
+    db
+      .select(getTableColumns(UserTable))
+      .from(UserTable)
+      .leftJoin(PostingTable, eq(PostingTable.agency, UserTable.id))
+      .leftJoin(ApplicationTable, eq(ApplicationTable.posting, PostingTable.id))
+      .where(
+        and(
+          isNotNull(UserTable.photo),
+          isNotNull(UserTable.bio),
+          isNotNull(UserTable.instagramDetails),
+          isNotNull(UserTable.name),
+          isNotNull(UserTable.username),
+          eq(UserTable.role, Roles.Agency),
+        ),
+      )
+      .groupBy(UserTable.id)
+      .orderBy(desc(sql`COUNT(${ApplicationTable.id})`), UserTable.id),
+    {
+      page,
+      pageSize: 9,
+    },
+  );
 }
